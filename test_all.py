257fec76412583942585589b7a951491c5e31fc6
ktulhy <sarvash.inc@gmail.com>
2017-03-13 16:34:10 +0700

    Прикрутил тесты на exception-ы

--- test_all.py
+++ test_all.py
@@ -5,53 +5,61 @@ import pytest
 
 from br_compiler import FileCompiler, Lexer
 from executor import Interpreter
-from utils import get_tests
+from test_utils import BrTests, get_tests
 
 
-def file_execute(file_name, *args):
+def file_execute(file_name, test: BrTests):
     print(file_name)
-    memory_tests, memory_dict_test, inp, out = args
 
+    interpreter = None
     block = None
-    with open(file_name, 'rt') as f:
-        l = Lexer(f.readlines())
-
-        block = l.block
-
-    compiler = FileCompiler(file_name, block)
-
-    compiler.compile()
-
-    bytecode = compiler.context.full_bytecode()
-
-    program_out = io.StringIO()
-    interpreter = Interpreter(bytecode,
-                              output=program_out,
-                              inp=inp)
 
     try:
-        while True:
-            interpreter.step()
-    except EOFError:
-        pass
+        with open(file_name, 'rt') as f:
+            l = Lexer(f.readlines())
+
+            block = l.block
+
+        compiler = FileCompiler(file_name, block)
+
+        compiler.compile()
+
+        bytecode = compiler.context.full_bytecode()
+
+        program_out = io.StringIO()
+        interpreter = Interpreter(bytecode,
+                                  output=program_out,
+                                  inp=test.inp)
+
+        try:
+            while True:
+                interpreter.step()
+        except EOFError:
+            pass
+
+    except Exception as e:
+        assert e.__class__.__name__ == test.exc_name
+        print("Exception '{test.exc_name}' catched".format(test=test))
+        print(" ====== ")
+        return
 
     # tests
     mt_count = 0
-    for test in memory_tests:
-        for tv, mv in zip(test, interpreter.memory):
+    for value in test.memory:
+        for tv, mv in zip(value, interpreter.memory):
             if -1 != tv:
                 assert tv == mv
                 mt_count += 1
 
     print("Memory Tested {} cells, OK".format(mt_count))
 
-    assert interpreter.memory == memory_dict_test
+    assert interpreter.memory == test.memory_dict
 
-    print("Memory Dict Tested {} cells, OK".format(len(memory_dict_test)))
+    print("Memory Dict Tested {} cells, OK".format(len(test.memory_dict)))
 
-    if out:
+    if test.out:
         po_v = program_out.getvalue()
-        o_v = out.getvalue()
+        o_v = test.out.getvalue()
         assert po_v == o_v
         print("Output test OK, len: {}".format(len(o_v)))
 
@@ -62,5 +70,5 @@ def test_files():
     file_names = sorted(glob.glob("./test_files/*/*.br", recursive=True))
 
     for file_name in file_names:
-        args = get_tests(file_name)
-        file_execute(file_name, *args)
+        test = get_tests(file_name)
+        file_execute(file_name, test)
--- utils.py
+++ utils.py
@@ -1,33 +1,6 @@
-import io
-
-
 def clear_list(source: list):
     return [element for element in source if element]
 
 
 def _get_quote(s: str):
-    return s[s.find('"') + 1 : s.rfind('"')]
-
-
-def get_tests(file_name):
-    memory_tests = []
-    memory_dict_test = {}
-    inp = None
-    out = None
-    with open(file_name, 'rt') as f:
-        lines = f.readlines()
-        for line in lines:
-            if "#!" != line[:2]:
-                continue
-            tokens = clear_list(line.split(" ")[1:])
-            if "test_MEMORYL" == tokens[0]:
-                memory_tests.append(
-                    [int(x) if x != "_" else -1 for x in tokens[1:]]
-                )
-            elif "test_MEMORYD" == tokens[0]:
-                memory_dict_test[int(tokens[1])] = int(tokens[2])
-            elif "test_INPUT" == tokens[0]:
-                inp = io.StringIO(_get_quote(line))
-            elif "test_OUTPUT" == tokens[0]:
-                out = io.StringIO(_get_quote(line))
-    return memory_tests, memory_dict_test, inp, out
+    return s[s.find('"') + 1: s.rfind('"')]
